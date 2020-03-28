04e5f45b393e8804efca0757dea06931bf1f5254
Nazareth <50680385+N4za@users.noreply.github.com>
2020-03-28 14:08:27 -0600

    Digits Recognition

---
+++ main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'mainscreen.dart';
+
+void main()=>runApp(recognition());
+
+class recognition extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Colors.blue,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: mainScreen(),
+    );
+  }
+}
